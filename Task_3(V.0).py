be5eb4586bbda73f29bb2fad2de38e6097b81f6c
Egor <123231130+Darkness-Warrior@users.noreply.github.com>
2023-02-18 10:36:42 +0300

    Add files via upload

---
+++ Task_3(V.0).py
@@ -0,0 +1,37 @@
+class Player:
+    def __init__(self, movement, team, item):
+        self.__movement = movement
+        self.__team = team
+        self.__item = item
+
+    @property
+    def movement(self):
+        return self.__movement
+
+    @movement.setter
+    def movement(self, movement):
+        self.__movement = movement
+
+    def get_team(self):
+        return self.__team
+
+    def set_team(self, team):
+        self.__team = team
+
+    @property
+    def item(self):
+        return self.__item
+
+    def set_item(self):
+        return self.item
+
+
+player = Player("forward", "red team", "blue team flag")
+player2 = Player("on the flank", "blue team", "red team flag")
+
+print(player.movement)
+print(player.team)
+print(player.item, "\n")
+print(player2.movement)
+print(player2.team)
+print(player2.item)
